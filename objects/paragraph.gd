3fec2fba76ec4c35770a21c4b85d78b254b1b517
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2025-06-12 13:22:21 -0400

    Moved text formatting into own global class

--- objects/paragraph.gd
+++ objects/paragraph.gd
@@ -2,69 +2,13 @@ class_name Paragraph extends RichTextLabel
 
 const FONT = preload("res://objects/paragraph_font.tres")
 
-class Styling:
-	enum TYPES {
-		BOLD,
-		ITALIC
-	}
-	static func do(type:TYPES, is_open:bool = false) -> String:
-		match type:
-			TYPES.BOLD:
-				if is_open: return "[/b]"
-				else: return "[b]"
-			TYPES.ITALIC:
-				if is_open: return "[/i]"
-				else: return "[i]"
-			_:
-				push_error("Invalid enum integer")
-				return ""
-	static func replace(t:String, type:TYPES) -> String:
-		var f:String = t
-		
-		var special_character:String
-		if type == TYPES.BOLD:
-			special_character = "*"
-		elif type == TYPES.ITALIC:
-			special_character = "_"
-			
-		
-		var char:int = 0
-		while char != -1:
-			var is_open:bool = false
-			var next_char = f.find(special_character, char)
-			if next_char != -1:
-				# Note that length returns the number of characters but string[0] is character 1.
-				if f.length()-1 > next_char: # If this isn't the end
-					if f[next_char] == f[next_char+1]: # If the next character is also 
-						# Escaped
-						char = next_char + 2 # Skip past these characters
-						continue
-						
-				char = next_char
-				
-				var insert:String = Styling.do(type, is_open)
-				is_open = !is_open # Toggle
-				
-				# Replace the stuff
-				f = f.erase(next_char)
-				f = f.insert(next_char, insert)
-				
-				char += insert.length() - 1 # Math cus we erase one character before adding our insert
-				continue # Keep looping
-			char = -1
-		return f
-
-var is_visual_current:bool = true
-
 var _text:String:
 	set(value):
 		_text = value
-		is_visual_current = false
 		refresh_visuals()
 var style:Style:
 	set(value):
 		style = value
-		is_visual_current = false
 		refresh_visuals()
 
 func _prepare_text(t:String) -> String:
@@ -77,8 +21,8 @@ func _prepare_text(t:String) -> String:
 		elif style.text_align == "right":
 			f = f.insert(0, "[right]")
 	
-	f = Styling.replace(f, Styling.TYPES.BOLD)
-	f = Styling.replace(f, Styling.TYPES.ITALIC)
+	f = TextFormatting.replace(f, TextFormatting.TYPES.BOLD)
+	f = TextFormatting.replace(f, TextFormatting.TYPES.ITALIC)
 	
 	return f
 
@@ -96,7 +40,7 @@ func _init(titties:String, style:Style = null) -> void:
 
 func refresh_visuals() -> void:
 	text = _prepare_text(_text) # Because special formatting on PD
-	is_visual_current = true
+	
 	
 func to_dict() -> Dictionary:
 	var data:Dictionary = {
---
+++ objects/text_formatting.gd
@@ -0,0 +1,52 @@
+class_name TextFormatting
+
+enum TYPES {
+	BOLD,
+	ITALIC
+}
+static func do(type:TYPES, is_open:bool = false) -> String:
+	match type:
+		TYPES.BOLD:
+			if is_open: return "[/b]"
+			else: return "[b]"
+		TYPES.ITALIC:
+			if is_open: return "[/i]"
+			else: return "[i]"
+		_:
+			push_error("Invalid enum integer")
+			return ""
+static func replace(t:String, type:TYPES) -> String:
+	var f:String = t
+	
+	var special_character:String
+	if type == TYPES.BOLD:
+		special_character = "*"
+	elif type == TYPES.ITALIC:
+		special_character = "_"
+		
+	
+	var char:int = 0
+	while char != -1:
+		var is_open:bool = false
+		var next_char = f.find(special_character, char)
+		if next_char != -1:
+			# Note that length returns the number of characters but string[0] is character 1.
+			if f.length()-1 > next_char: # If this isn't the end
+				if f[next_char] == f[next_char+1]: # If the next character is also 
+					# Escaped
+					char = next_char + 2 # Skip past these characters
+					continue
+					
+			char = next_char
+			
+			var insert:String = TextFormatting.do(type, is_open)
+			is_open = !is_open # Toggle
+			
+			# Replace the stuff
+			f = f.erase(next_char)
+			f = f.insert(next_char, insert)
+			
+			char += insert.length() - 1 # Math cus we erase one character before adding our insert
+			continue # Keep looping
+		char = -1
+	return f
