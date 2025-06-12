732fb6374d40a2a5bdd22d1b5fccce9c7e01cbcc
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2025-06-12 18:52:11 -0400

    Image scale is read/set on instantiation

--- objects/image.gd
+++ objects/image.gd
@@ -1,5 +1,7 @@
 class_name ConstellationImage extends Control
 
+const TEXTURE = preload("res://icon.svg")
+
 const TEMPLATE = {
 	"type": "image",
 	"width": "0",
@@ -19,33 +21,37 @@ var _pixels:String:
 	set(value):
 		_pixels = value
 		refresh_visuals()
-var style:Style:
+var style:Style = Style.new():
 	set(value):
 		style = value
 		refresh_visuals()
 
-func _init(width:int, height:int, pixels:String) -> void:
+func _init(width:int, height:int, pixels:String, rescale:int = 1) -> void:
 	self._width = maxi(width, 0)
 	self._height = maxi(height, 0)
 	self._pixels = pixels
 	
+	if rescale > 1: self.style.scale = rescale
+	
 	self.focus_mode = Control.FOCUS_CLICK
 	
 func _draw() -> void:
-	draw_rect(
+	draw_texture_rect(
+		TEXTURE,
 		Rect2i(
-			centered(_width),
+			centered(_width*style.scale),
 			0,
-			_width,
-			_height
+			_width*style.scale,
+			_height*style.scale
 		),
-		Color.YELLOW
+		false
+		#Color.YELLOW
 	)
 	
 func centered(width:int) -> int: return 400/2 - width/2
 
 func refresh_visuals() -> void:
-	custom_minimum_size = Vector2i(_width, _height)
+	custom_minimum_size = Vector2i(_width*style.scale, _height*style.scale)
 	queue_redraw()
 	#_data_to_pixels()
 	
